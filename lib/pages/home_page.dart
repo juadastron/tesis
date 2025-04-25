09f832b8d05414739f4299b3a28ae48e89d5950b
juan <juan.d.carreno@unl.edu.ec>
2025-04-25 02:12:37 -0500

    nueva interfaz del login

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,22 +4,37 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import '../providers/user_provider.dart';
 
-
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-  final user = Provider.of<UserProvider>(context);
-  final String? userRole = user.rol; // Rol deeesde la API
+    final user = Provider.of<UserProvider>(context);
+    final String? userRole = user.rol;
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Geo Little Paws'),
+        title: Text(
+          'Geo Little Paws',
+          style: GoogleFonts.montserrat(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: const Color(0xFF6A1B9A),
       ),
-      body: Padding(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 255, 255, 255),
+              Color(0xFFFFF7D4),
+            ],
+          ),
+        ),
         padding: const EdgeInsets.all(20),
         child: GridView.count(
           crossAxisCount: 2,
@@ -30,7 +45,7 @@ class HomePage extends StatelessWidget {
               context,
               icon: FontAwesomeIcons.mapLocationDot,
               label: 'Ver ubicación',
-              color: Colors.deepPurple,
+              color: const Color(0xFF6A1B9A),
               onTap: () {
                 Navigator.pushNamed(context, '/mapa');
               },
@@ -40,7 +55,7 @@ class HomePage extends StatelessWidget {
                 context,
                 icon: FontAwesomeIcons.user,
                 label: 'Usuarios',
-                color: Colors.teal,
+                color: const Color(0xFFBA68C8),
                 onTap: () {
                   Navigator.pushNamed(context, '/usuarios');
                 },
@@ -50,7 +65,7 @@ class HomePage extends StatelessWidget {
                 context,
                 icon: FontAwesomeIcons.paw,
                 label: 'Animales',
-                color: Colors.orange,
+                color: const Color(0xFFFFD54F),
                 onTap: () {
                   Navigator.pushNamed(context, '/animales');
                 },
@@ -60,7 +75,7 @@ class HomePage extends StatelessWidget {
                 context,
                 icon: FontAwesomeIcons.microchip,
                 label: 'Dispositivos',
-                color: Colors.indigo,
+                color: const Color(0xFF42A5F5),
                 onTap: () {
                   Navigator.pushNamed(context, '/dispositivos');
                 },
@@ -68,6 +83,7 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      
     );
   }
 
@@ -76,39 +92,43 @@ class HomePage extends StatelessWidget {
       required String label,
       required Color color,
       required VoidCallback onTap}) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: color.withOpacity(0.9),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: color.withOpacity(0.4),
-              blurRadius: 6,
-              offset: const Offset(2, 4),
-            )
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FaIcon(
-              icon,
-              color: Colors.white,
-              size: 36,
-            ),
-            const SizedBox(height: 10),
-            Text(
-              label,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.montserrat(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        onTap: onTap,
+        child: Ink(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 6,
+                offset: const Offset(2, 4),
+              )
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FaIcon(
+                icon,
                 color: Colors.white,
+                size: 36,
               ),
-            )
-          ],
+              const SizedBox(height: 10),
+              Text(
+                label,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
